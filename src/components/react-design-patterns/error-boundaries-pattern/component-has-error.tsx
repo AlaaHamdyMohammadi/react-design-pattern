a49dcc87b42e41a8b849be232b2a1ca816229b8b
AlaaHamdy <alaahamdy2197@gmail.com>
2024-12-21 20:55:47 +0200

    feat error boundary pattern

---
+++ src/components/react-design-patterns/error-boundaries-pattern/component-has-error.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ComponentHasError = () => {
+  return (
+    <div>
+      this pattern for show the error messeges when something goes wrong in fallback component
+    </div>
+  )
+}
+
+export default ComponentHasError
